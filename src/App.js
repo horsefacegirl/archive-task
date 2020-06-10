9b3297a20d59280a6ff518d2e19709a15b9cb51f
horsefacegirl <51134090+horsefacegirl@users.noreply.github.com>
2020-06-10 16:38:22 +0200

    update readme, formatting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 export const App = () => {
-  const urlFiles= "http://localhost:3000/files";
+  const urlFiles = "http://localhost:3000/files";
   const [files, setFiles] = useState([]);
   const getFiles = () => {
     fetch(urlFiles)
-      .then(res => res.json())
-      .then(json => setFiles(json));
+      .then((res) => res.json())
+      .then((json) => setFiles(json));
   };
 
   useEffect(() => {
@@ -15,60 +15,60 @@ export const App = () => {
   }, []);
 
   const getDate = (time) => {
-const date = new Date(time)
-return date.toLocaleDateString("sv-SE")
-  }
-  
+    const date = new Date(time);
+    return date.toLocaleDateString("sv-SE");
+  };
+
   return (
     <AppWrapper>
       <FlexHeader>
-      <h1>File Archive</h1>
-    <Button>New file</Button>
-    </FlexHeader>
-    <Flex>
-      <FlexChild bold>Name</FlexChild>
+        <h1>File Archive</h1>
+        <Button>New file</Button>
+      </FlexHeader>
+      <Flex>
+        <FlexChild bold>Name</FlexChild>
         <FlexChild bold>Description</FlexChild>
         <FlexChild bold>Type</FlexChild>
         <FlexChild bold>Created at</FlexChild>
-      {files && files.map(file => (
-        <>
-        <FlexChild>{file.name}</FlexChild>
-        <FlexChild>{file.description}</FlexChild>
-        <FlexChild>{file.type}</FlexChild>
-        <FlexChild>{getDate(file.createdAt)}</FlexChild>
-        </>
-      ))}
-    </Flex>
+        {files &&
+          files.map((file) => (
+            <>
+              <FlexChild>{file.name}</FlexChild>
+              <FlexChild>{file.description}</FlexChild>
+              <FlexChild>{file.type}</FlexChild>
+              <FlexChild>{getDate(file.createdAt)}</FlexChild>
+            </>
+          ))}
+      </Flex>
     </AppWrapper>
-  )}
+  );
+};
 
 const AppWrapper = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 const FlexHeader = styled.div`
-display: flex;
-justify-content: space-around;
-align-items: baseline;
-width: 50%;
-
-`
-
+  display: flex;
+  justify-content: space-around;
+  align-items: baseline;
+  width: 50%;
+`;
 const Button = styled.button`
-padding: 0.5rem 1rem;
-border-radius: 20px;
-`
+  padding: 0.5rem 1rem;
+  border-radius: 20px;
+`;
 
 const Flex = styled.div`
-display: flex;
-flex-wrap: wrap;
-width: 50%;
-`
+  display: flex;
+  flex-wrap: wrap;
+  width: 50%;
+`;
 const FlexChild = styled.div`
-width: 20%;
-padding: 0.5rem;
-border: 1px solid black;
-font-weight: ${props => props.bold ? 700 : 400 }
-`
+  width: 20%;
+  padding: 0.5rem;
+  border: 1px solid black;
+  font-weight: ${(props) => (props.bold ? 700 : 400)};
+`;
